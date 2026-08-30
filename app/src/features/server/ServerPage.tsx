e7a00c238700a8969fa3527c8a538a2726dd78a5
Sergei Ronzhin <sergei.ronzhin@salote.ch>
2026-08-30 03:53:23 +0300

    add server map tab with resource-tile bonuses around the Royal City

--- app/src/features/server/ServerPage.tsx
+++ app/src/features/server/ServerPage.tsx
@@ -3,10 +3,12 @@ import { SectionHeader } from '../../components/layout/SectionHeader';
 import { TabBar } from '../../components/layout/TabBar';
 import { AlliancesTab } from './tabs/AlliancesTab';
 import { GeneralInfoTab } from './tabs/GeneralInfoTab';
+import { MapTab } from './tabs/MapTab';
 import { RulesTab } from './tabs/RulesTab';
 
 const TABS = [
   { to: '/server/info', label: 'Общая информация' },
+  { to: '/server/map', label: 'Карта' },
   { to: '/server/rules', label: 'Правила' },
   { to: '/server/alliances', label: 'Союзы' },
 ];
@@ -19,6 +21,7 @@ export function ServerPage() {
       <Routes>
         <Route index element={<Navigate to="info" replace />} />
         <Route path="info" element={<GeneralInfoTab />} />
+        <Route path="map" element={<MapTab />} />
         <Route path="rules" element={<RulesTab />} />
         <Route path="alliances" element={<AlliancesTab />} />
       </Routes>
